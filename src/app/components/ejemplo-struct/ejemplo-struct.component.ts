94c6c3d3dca33f6d4944729b6a9e2ea1a5851ba0
ErikaJPB <erikajpinedab@gmail.com>
2023-12-05 15:11:01 -0500

    structural directives

--- src/app/components/ejemplo-struct/ejemplo-struct.component.ts
+++ src/app/components/ejemplo-struct/ejemplo-struct.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-ejemplo-struct',
   templateUrl: './ejemplo-struct.component.html',
-  styleUrls: ['./ejemplo-struct.component.scss']
+  styleUrls: ['./ejemplo-struct.component.scss'],
 })
 export class EjemploStructComponent {
+  rol: boolean = false;
 
+  cambiarRol() {
+    this.rol = !this.rol;
+  }
 }
--- src/app/directives/struct.directive.ts
+++ src/app/directives/struct.directive.ts
@@ -1,10 +1,25 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
-  selector: '[appStruct]'
+  selector: '[rolCorrecto]',
 })
 export class StructDirective {
+  private _mostrando: boolean = false;
 
-  constructor() { }
+  @Input() set rolCorrecto(condicion: boolean) {
+    if (condicion && !this._mostrando) {
+      // Creamos el elemento
+      this._viewContainerRef.createEmbeddedView(this._templateRef);
+      this._mostrando = true;
+    } else {
+      // Eliminamos el elemento
+      this._viewContainerRef.clear();
+      this._mostrando = false;
+    }
+  }
 
+  constructor(
+    private _templateRef: TemplateRef<any>,
+    private _viewContainerRef: ViewContainerRef
+  ) {}
 }
